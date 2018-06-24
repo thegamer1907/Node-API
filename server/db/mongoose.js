5b74c370c73d95281696bdba1520748134b18b96
Harshit Agarwal <harshitagar1907@gmail.com>
2018-06-24 22:22:39 +0530

    Added Custom Database URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,16 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://thegamer1907:abc123***@ds161610.mlab.com:61610/todotry');
+var murl = '';
+
+if(process.env.NODE_ENV) {
+  murl = 'mongodb://thegamer1907:abc123***@ds161610.mlab.com:61610/todotry';
+}
+else {
+  murl = 'mongodb://localhost:27017/TodoApp';
+}
+
+mongoose.connect(murl);
 
 module.exports = {
   mongoose: mongoose
